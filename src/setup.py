5c8e3a8e6d3d17c2e7eabc8527ddd184a9574c08
Wayne Wang <wangwenyu0928@gmail.com>
2022-10-21 22:41:51 +0800

    Update src and script folders

--- src/setup.py
+++ src/setup.py
@@ -4,14 +4,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='RECASOpt',
-    version='0.0.1',
-    author='Wenyu Wang',
-    author_email='wenyu_wang@u.nus.edu',
-    description='An surrogate-assisted optimizer for expensive many-objective optimization problems',
+    name="RECASOpt",
+    version="0.0.1",
+    author="Wenyu Wang",
+    author_email="wenyu_wang@u.nus.edu",
+    description="An surrogate-assisted optimizer for expensive many-objective optimization problems",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='https://github.com/WY-Wang/2021.0343/src/RECASOpt',
-    license='MIT',
+    url="https://github.com/WY-Wang/RECASOpt",
+    license="MIT",
     packages=setuptools.find_packages(),
+    install_requires=["six"],
 )
